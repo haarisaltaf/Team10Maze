77748444b648a3ace562a08aa89035bee7ed3834
haarisaltaf <altafhaaris@gmail.com>
2025-10-07 15:11:34 +0100

    added readme stuff and commented game file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Team10Maze
 Maze game created using Java for ENG1.
+
+to run:
+cd into this directory and type "./gradlew run" -- if on windows: "./gradlew.bat run"
+
+Relative path for the game.java itself (the game is stored here)
+#### core/src/main/java/com/Team10/Team10Maze/mazeGame.java
--- core/src/main/java/com/Team10/Team10Maze/mazeGame.java
+++ core/src/main/java/com/Team10/Team10Maze/mazeGame.java
@@ -1,34 +1,38 @@
 package com.Team10.Team10Maze;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+
+import com.badlogic.gdx.ApplicationAdapter; // application window class
+import com.badlogic.gdx.Gdx; // gives direct edits to application window
+import com.badlogic.gdx.graphics.GL20; // controls openGL drawing
+import com.badlogic.gdx.graphics.Texture; // image object class -- draws .jpeg/ .png
+import com.badlogic.gdx.graphics.g2d.SpriteBatch; // how gdx draws 2d images efficently
+import com.badlogic.gdx.utils.ScreenUtils; // useful library for screen tasks eg clearing screen
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class mazeGame extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class mazeGame extends ApplicationAdapter { // inheriting Application class
+    private SpriteBatch batch; // new drawing object
+    private Texture image; // image object
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
+        batch = new SpriteBatch(); // new creating batch image
         image = new Texture("libgdx.png");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f); // clears image
+        batch.begin(); // draws the image
         batch.draw(image, 140, 210);
         batch.end();
     }
 
     @Override
     public void dispose() {
+// cleanup on exit (from gpu) as garbage collector doesnt work on gpu (applies to memory)
         batch.dispose();
         image.dispose();
     }
 }
+
+// TODO: add main menu screen -- buttons and background.jpeg
