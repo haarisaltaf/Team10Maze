bec4cd7f90c29adb0f97127b46374eebea2b8345
haarisaltaf <altafhaaris@gmail.com>
2025-10-08 04:41:37 +0100

    Created square filling-background for menu

--- core/src/main/java/com/Team10/Team10Maze/mazeGame.java
+++ core/src/main/java/com/Team10/Team10Maze/mazeGame.java
@@ -12,12 +12,14 @@ import com.badlogic.gdx.graphics.Texture; // image object class -- draws .jpeg/ 
 import com.badlogic.gdx.graphics.g2d.TextureRegion; // Allows for subsection of a jpeg to be taken
 import com.badlogic.gdx.graphics.g2d.SpriteBatch; // how gdx draws 2d images efficently
 
-
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class mazeGame extends ApplicationAdapter { // inheriting Application class
     private SpriteBatch batch; // new drawing object
     private Texture dungeonGroundsTexture;
     private TextureRegion menuBackground; // image object
+    private float[] lengthxy;
+    // TODO: change to TextureRegion[][] ... = TextureRegion.split() to auto split the entire image evenly into tiles - sort out once more than just the background is needed
+
 
     @Override
     public void create() {
@@ -25,14 +27,16 @@ public class mazeGame extends ApplicationAdapter { // inheriting Application cla
 
         // hold the grounds texture to then extract the region with the menuBackground
         dungeonGroundsTexture = new Texture("SGQ_Dungeon/grounds_and_walls/grounds.png");
-        menuBackground = new TextureRegion(dungeonGroundsTexture, 64, 256, 64, 64); // at (64, 256) on ground.png take a region of 64x64
+        menuBackground = new TextureRegion(dungeonGroundsTexture, 64, 192, 64, 64); // at (64, 256) on ground.png take a region of 64x64
+        lengthxy = getBackgroundSize();
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f); // clears image
         batch.begin();
-        batch.draw(menuBackground, 64, 64);  // draws the image
+        // batch.draw(wallRegion, x, y, smallest dimenstion, smallest dimenstion)
+        batch.draw(menuBackground, lengthxy[1], lengthxy[2], lengthxy[0], lengthxy[0]);  // draws the image
         batch.end();
     }
 
@@ -41,6 +45,21 @@ public class mazeGame extends ApplicationAdapter { // inheriting Application cla
 // cleanup on exit (from gpu) as garbage collector doesnt work on gpu (applies to memory)
         batch.dispose();
         dungeonGroundsTexture.dispose();
+    }
+
+    public float[] getBackgroundSize() {
+        // TODO: making a function that can calculate the correct size of the square background image.
+
+        int screenWidth = Gdx.graphics.getWidth();
+        int screenHeight = Gdx.graphics.getHeight();
+
+        // calc max-length to strech without overscanning, use smallest dimension:
+        float length = Math.min(screenWidth, screenHeight);
+        float x = (screenWidth - length) / 2f;
+        float y = (screenHeight - length) / 2f;
+
+        float[] returnValue = {length, x, y};
+        return returnValue;
     }
 }
 
