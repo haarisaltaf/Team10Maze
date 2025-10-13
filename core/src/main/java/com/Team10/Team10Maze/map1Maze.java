098141b9464aa69343a93b48c05c480823d18a47
haarisaltaf <altafhaaris@gmail.com>
2025-10-13 06:25:47 +0100

    added map1Maze placeholder to be added to

---
+++ core/src/main/java/com/Team10/Team10Maze/map1Maze.java
@@ -0,0 +1,38 @@
+package com.Team10.Team10Maze;
+
+
+// Drawing and texture imports
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20; // controls openGL drawing
+import com.badlogic.gdx.graphics.Texture; // image object class -- draws .jpeg/ .png
+import com.badlogic.gdx.graphics.g2d.TextureRegion; // Allows for subsection of a jpeg to be taken
+import com.badlogic.gdx.graphics.g2d.SpriteBatch; // how gdx draws 2d images efficently
+
+// stage, ui elements etc
+import com.badlogic.gdx.scenes.scene2d.ui.Button; // import button
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+// Application and Window class imports
+import com.badlogic.gdx.Gdx; // gives direct edits to application window
+import com.badlogic.gdx.utils.ScreenUtils; // useful library for screen tasks eg clearing screen
+
+
+public class map1Maze implements Screen {
+
+    // public map1Maze(mazeGame game) {
+    //     // passes through the superclass's (main Game) "this" instance
+    //     this.game = game;
+    // }
+
+    @Override public void show() { }
+    @Override public void dispose() { }
+    @Override public void render(float delta) { }
+    @Override public void hide() { }
+    @Override public void resize(int width, int height) {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+}
