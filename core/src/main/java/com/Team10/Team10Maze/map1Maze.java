dd3b86caaca2ec72de82743dcf75747405465faa
diyarsavdagh <diyarsavda@gmail.com>
2025-10-14 14:32:41 +0100

    Add movement for all 4 keys

--- core/src/main/java/com/Team10/Team10Maze/map1Maze.java
+++ core/src/main/java/com/Team10/Team10Maze/map1Maze.java
@@ -26,6 +26,8 @@ public class map1Maze implements Screen {
     private SpriteBatch batch;
     private BitmapFont font;
     private float timeLeft = 3f;
+    private int maxX = 33;
+    private int maxY = 31;
 
     // -------- TEST MAP FOR NOW -------- allows for us to sort out the movement and display for now whilst waiting for proper maze to be done then can replace
     // KEY: "." = open path, "#" = wall, "G" for goal
@@ -58,12 +60,12 @@ public class map1Maze implements Screen {
         "#.#.#.....................#.#...#",
         "#.#.#####################.#.###.#",
         "#.#.......................#.....#",
-        "#.#############################.#",
+        "#.############.....############.#",
         "#...............P...............#",
         "#################################"
     ));
     public int[] playerLocation = {16, 29}; // x, y
-    public int[] goalLocation = {2, 1}
+    public int[] goalLocation = {2, 1};
 
 
     public map1Maze(mazeGame game) {
@@ -76,7 +78,9 @@ public class map1Maze implements Screen {
         addTime(5f);
     }
 
-    @Override public void show() { }
+    @Override public void show() {
+        map.forEach( (n) -> { System.out.println(n);} );
+    }
     @Override public void dispose() {
         font.dispose();
         batch.dispose();
@@ -110,81 +114,93 @@ public class map1Maze implements Screen {
     // TODO: CHECK ALL OF THESE INPUTS AND HANDLE THEM CORRECTLY -- Refer to W as this is done relatively ok for now: few edge cases may be funky
     // Also need to handle when reaching goal -- store where goal is then check if playerLocation == goalLocation every movement?
     public void handleInput() {
-
+        
         if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {
             System.out.println("W");
-            moveCharacter('W');
-            map.forEach( (n) -> { System.out.println(n);} );
+            moveCharacter('W', 0, -1);
         }
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.A)) {
             System.out.println("A");
-            if (playerLocation[0] <= 0) { // TODO: move to within moveCharacter method like when handling W
-                return;
-            }
-            moveCharacter('A');
-            playerLocation[0] -= 1;
+            moveCharacter('A', 1, 0);
         }
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.S)) {
             System.out.println("S");
-            if (playerLocation[1] >= 31) {
-                return;
-            }
-            moveCharacter('S');
-            playerLocation[1] += 1;
+            moveCharacter('S',  0, 1);
         }
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.D)) {
             System.out.println("D");
-            if (playerLocation[0] >= 32) {
-                return;
-            }
-            moveCharacter('D');
-            playerLocation[0] += 1;
+            moveCharacter('D', -1, 0);
         }
 
     }
 
-    public void moveCharacter(char letter){
+    public void moveCharacter(char letter, int xDifference, int yDifference){
         // letter == direction to move, checks if wall there and returns the same if so.
         // If no wall, then move the player in that direction and return new map.
         // If G, then overlap and set as winner.
-        if (letter == 'W') {
-            String lineAbove = map.get(playerLocation[1]-1);
-            String currLine = map.get(playerLocation[1]);
+        if (yDifference != 0) {
 
-            // ----- dont move if wall -----
-            if (lineAbove.charAt(playerLocation[0]) == '#' || playerLocation[1] <= 0){
+            // Get the line above the player's current line
+            String lineAbove = map.get(playerLocation[1]+yDifference);
+            if (lineAbove == null) {
+                // Return if that line doesn't exist
                 return;
             }
 
-            // ----- format currLine -----
+            String currLine = map.get(playerLocation[1]);
+            if (lineAbove.charAt(playerLocation[0]) == '#'){
+                // Return if they are trying to go through a wall
+                return;
+            }
+
+            // Replace the P with a . and set the current line without the P
+            currLine = currLine.replace('P', '.');
+            map.set(playerLocation[1], currLine);
+
+            // Replace the player's X coordinate with a P on their new Y coordinate
+            StringBuilder newLineAbove = new StringBuilder(lineAbove);
+            newLineAbove.setCharAt(playerLocation[0], 'P');
+            map.set(playerLocation[1]+yDifference, newLineAbove.toString());
+
+            playerLocation[1] += yDifference;
+            if (playerLocation == goalLocation){
+                goalReached();
+            }
+
+            map.forEach( (n) -> { System.out.println(n);} );
+            return;
+        }
+
+
+        if (xDifference != 0) {
+
+            // Get player's current line
+            String currLine = map.get(playerLocation[1]);
+            if (playerLocation[0]+xDifference > 33 || currLine.charAt(playerLocation[0]+xDifference) == '#') {
+                // Checks if they are past the limit or going into a wall
+                return;
+            }
+
             currLine = currLine.replace('P', '.');
             map.set(playerLocation[1], currLine);
 
 
             // ----- format lineAbove -----
-            StringBuilder newLineAbove = new StringBuilder(lineAbove);
-            newLineAbove.setCharAt(playerLocation[0], 'P');
-            map.set(playerLocation[1]-1, newLineAbove.toString());
+            StringBuilder newCurrLine = new StringBuilder(currLine);
+            newCurrLine.setCharAt(playerLocation[0]+xDifference, 'P');
+            map.set(playerLocation[1], newCurrLine.toString());
 
             // ---- Updating current location of where player is
-            playerLocation[1] -= 1;
+            playerLocation[0] += xDifference;
             if (playerLocation == goalLocation){
-                goalReached;
+                goalReached();
             }
-        }
 
-        // TODO: handle movement for A and S and D
-        if (letter == 'A') {
-
-        }
-        if (letter == 'S') {
-
-        }
-        if (letter == 'D') {
-
+            map.forEach( (n) -> { System.out.println(n);} );
+            return;
         }
     }
 
