2eeec5fdfac0cdbf8d04a39c86bd7e93d4986679
haarisaltaf <altafhaaris@gmail.com>
2025-11-10 04:57:34 +0000

    new maze being made

---
+++ assets/maps/classFloor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="classFloor" tilewidth="248" tileheight="145" tilecount="1" columns="1">
+ <image source="../split2dClassroom/wallsetc/walls__2.png" trans="ff00ff" width="253" height="156"/>
+</tileset>
---
+++ assets/maps/wallsDoorsFloors.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="wallDoorFloors" tilewidth="32" tileheight="32" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="18">
+  <image source="../split2dClassroom/resizedWalls/wall_90.png" width="32" height="32"/>
+ </tile>
+ <tile id="19">
+  <image source="../split2dClassroom/resizedWalls/wall_180.png" width="32" height="32"/>
+ </tile>
+ <tile id="20">
+  <image source="../split2dClassroom/resizedWalls/wall_270.png" width="32" height="32"/>
+ </tile>
+ <tile id="21">
+  <image source="../split2dClassroom/resizedWalls/walls__1_imresizer.png" width="32" height="32"/>
+ </tile>
+ <tile id="22">
+  <image source="../split2dClassroom/resizedWalls/walls__2_32x32.png" width="32" height="32"/>
+ </tile>
+ <tile id="23">
+  <image source="../split2dClassroom/resizedWalls/walls__4_32x32.png" width="32" height="32"/>
+ </tile>
+ <tile id="24">
+  <image source="../split2dClassroom/resizedWalls/walls__7_32x32.png" width="32" height="32"/>
+ </tile>
+ <tile id="25">
+  <image source="../split2dClassroom/resizedWalls/walls__9_32x32.png" width="32" height="32"/>
+ </tile>
+ <tile id="26">
+  <image source="../split2dClassroom/resizedWalls/walls__9_fullgrey_32x32.png" width="32" height="32"/>
+ </tile>
+</tileset>
